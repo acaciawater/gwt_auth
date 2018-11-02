a73c0b88b32c2ff490f18a4fc63fbb639cb18848
theo <tkleinen@gmail.com>
2018-11-02 20:34:55 +0100

    1st commit

---
+++ src/components/Layer.vue
@@ -0,0 +1,20 @@
+<template>
+  <li class="layer"><a v-bind:href="layer.url">{{layer.name}}</a></li>
+</template>
+
+<script>
+export default {
+  name: "Layer",
+  props: ['layer']
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+li {
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/components/Map.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="map">
+    <h3>{{name}}</h3>
+    <ul>
+      <Layer v-for="item in layers" v-bind:layer="item" :title="item.url" :key="item.id">
+      </Layer>
+    </ul>
+  </div>
+</template>
+
+<script>
+import Layer from "./Layer.vue";
+export default {
+  name: "Map",
+  components: { Layer },
+  props: {
+    name: String
+  },
+  data() {
+    return {
+      layers: [
+        { id: 0, name: "Stress", url: "https://example.com" },
+        { id: 1, name: "Recharge", url: "https://example.com" },
+        { id: 2, name: "Landuse", url: "https://example.com" }
+      ]
+    };
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+</style>
